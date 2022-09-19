8167dcddff13577df64ca0275ff60dc4cef70758
emadam <ldy9037@naver.com>
2022-09-20 08:29:43 +0900

    feat: 타입 유효성 검증 추가

--- src/main/java/domain/attendance/Attendance.java
+++ src/main/java/domain/attendance/Attendance.java
@@ -1,18 +1,32 @@
 package domain.attendance;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Attendance {
     
-    private static final char[] TYPE = {'A', 'L', 'P'};
+    private static final char[] TYPES = {'A', 'L', 'P'};
 
     private final char attendanceType;
 
     Attendance(char attendanceType) {
+        validate(attendanceType);
         this.attendanceType = attendanceType;
     }
 
     public char getType() {
         return attendanceType;
+    }
+
+    private void validate(char type) {
+        if (!isValid(type)) {
+            throw new IllegalArgumentException("출석 유형은 A,L,P 중 하나여야 합니다.");
+        }
+    }
+
+    private boolean isValid(char type) {
+        return IntStream.range(0, TYPES.length)
+                        .mapToObj(i -> TYPES[i])
+                        .anyMatch(c -> c == type);
     }
 }
--- src/test/java/domain/attendance/AttendanceTest.java
+++ src/test/java/domain/attendance/AttendanceTest.java
@@ -9,7 +9,7 @@ public class AttendanceTest {
 
     @Test
     @DisplayName("출석을 기록한다.")
-    void createAttendance() {
+    void create() {
         // given 
         char typeName = 'A';
 
@@ -18,6 +18,18 @@ public class AttendanceTest {
 
         // then 
         assertThat(attendance.getType()).isEqualTo(typeName);
+    }
+
+    @Test
+    @DisplayName("출석을 잘못 기입할 경우 에러가 발생한다.")
+    void createWithNotExistType() {
+        // given
+        char typeName = 'D';
+
+        // when & then
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                                    .isThrownBy(() -> new Attendance(typeName))
+                                    .withMessageMatching("출석 유형은 A,L,P 중 하나여야 합니다.");
     }
 
 }

b4b2a1ffe4997368065949c6faa6b4543a969ee2
emadam <ldy9037@naver.com>
2022-09-23 08:12:27 +0900

    feat: 최소 지각 횟수 조건 추가

--- src/main/java/domain/attendance/Result.java
+++ src/main/java/domain/attendance/Result.java
@@ -1,6 +1,8 @@
 package domain.attendance;
 
 public class Result {
+
+    public static int ALLOWED_ABSENCE_COUNT = 3;
     
     private final ScoreCount score;
     private final Absence absence;
@@ -11,7 +13,15 @@ public class Result {
     }
 
     public int getScore() {
+        if (isExceededAbsences()) {
+            return 0;
+        }
+        
         return score.getCount();
+    }
+
+    private boolean isExceededAbsences() {
+        return (absence.getCount() > ALLOWED_ABSENCE_COUNT);
     }
 
 }
--- src/test/java/domain/attendance/ResultTest.java
+++ src/test/java/domain/attendance/ResultTest.java
@@ -3,6 +3,7 @@ package domain.attendance;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -17,5 +18,17 @@ public class ResultTest {
 
         // then
         assertThat(result.getScore()).isEqualTo(expected);
+    }   
+
+    @ParameterizedTest(name = "{displayName} - {arguments}")
+    @ValueSource(ints = {4, 10, 3})
+    @DisplayName("최소 지각 횟수를 초과할 경우 0점을 반환한다.")
+    void getScoreWithOverAbsenceCount(int score) {
+        // when 
+        Result result = new Result(
+                            new ScoreCount(score), 
+                            new Absence(Result.ALLOWED_ABSENCE_COUNT + 1));
+
+        assertThat(result.getScore()).isEqualTo(0);
     }
 }

1c2a4bc3fc2f67f4265342f404ff5e97eff6850b
emadam <ldy9037@naver.com>
2022-09-26 19:08:33 +0900

    test: attendances 초기화를 BeforeEach로 분리

--- src/test/java/domain/attendance_v2/AttendacnesTest.java
+++ src/test/java/domain/attendance_v2/AttendacnesTest.java
@@ -1,5 +1,6 @@
 package domain.attendance_v2;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -8,14 +9,22 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.Assertions.*;
 
 public class AttendacnesTest {
+
+    private Attendances attendances;
+
+    @BeforeEach
+    void setUp() {
+        this.attendances = new Attendances(new AttendanceFactory());
+    }
+
     @ParameterizedTest(name = "{displayName} - {arguments}")
     @CsvSource(value = {"AAA:3", "APA:1"}, delimiter = ':')
     @DisplayName("점수를 계산한다.")
     void calculate(String history, int expected) {
         // when
-        Attendances attendances = new Attendances(new AttendanceFactory());
-        
+        int score = attendances.calculate(history);
+
         // then
-        assertThat(attendances.calculate(history)).isEqualTo(expected);
+        assertThat(score).isEqualTo(expected);
     }
 }

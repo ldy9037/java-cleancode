b46e7ade6ea085c0e8aed036096968370207301c
emadam <ldy9037@naver.com>
2022-09-26 20:08:19 +0900

    refactoring: 점수 계산에 대한 책임을 Attendances에게 위임하고 서로 협력하도록 변경

--- src/main/java/domain/attendance_v2/AttendanceBook.java
+++ src/main/java/domain/attendance_v2/AttendanceBook.java
@@ -1,30 +1,22 @@
 package domain.attendance_v2;
 
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class AttendanceBook {
     public static final int MAX_ALLOW_ABSENCE = 3;
 
-    private final List<List<Character>> book;
+    private final List<String> book;
+    private final Attendances attendances = new Attendances(new AttendanceFactory());
 
-    AttendanceBook(List<List<Character>> book) {
+    AttendanceBook(List<String> book) {
         this.book = book;
     }
 
-    public static AttendanceBook of(String[] attendanceArr) {
-        List<List<Character>> book = Stream.of(attendanceArr)
-                                            .map(AttendanceBook::stringToCharacter)
-                                            .collect(Collectors.toList());
+    public static AttendanceBook of(String... attendanceArr) {
+        List<String> book = Arrays.asList(attendanceArr);
         
         return new AttendanceBook(book);
-    }
-
-    private static List<Character> stringToCharacter(String str) {
-        return str.chars()
-                    .mapToObj(i -> (char)i)
-                    .collect(Collectors.toList());
     }
 
     public int count() {
@@ -32,31 +24,6 @@ public class AttendanceBook {
     }
     
     public int getScore(Integer id) {
-        return calculate(book.get(id));
-    }
-
-    private int calculate(List<Character> attendances) {
-        int score = 0;
-        int absence = 0;
-
-        for (Character attendance : attendances) {
-            if (attendance == 'A') {
-                score += 1;
-            }
-            if (attendance == 'P') {
-                score -= 1;
-                absence += 1;
-            }
-        }
-        
-        if (absence > MAX_ALLOW_ABSENCE) {
-            score = 0;
-        }
-
-        if (score < 0 ) {
-            score = 0;
-        }
-
-        return score;
+        return attendances.calculate(book.get(id));
     }
 }
--- src/test/java/domain/attendance_v2/AttendanceBookTest.java
+++ src/test/java/domain/attendance_v2/AttendanceBookTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.stream.Stream;
-
 public class AttendanceBookTest {
 
     @Test
@@ -36,39 +34,5 @@ public class AttendanceBookTest {
         for (int i = 0; i < attendances.length; i++) {
             assertThat(attendanceBook.getScore(i)).isEqualTo(expected[i]);
         }
-    }
-
-    @Test
-    @DisplayName("결석 횟수가 최대 허용 값을 초과하면 0점 처리한다.")
-    void getScoreWithMaxiumAbsence() {
-        // given 
-        String absence = generateAttendances("P", AttendanceBook.MAX_ALLOW_ABSENCE + 1);
-        String attendance = generateAttendances("A", AttendanceBook.MAX_ALLOW_ABSENCE + 2);
-        String[] attendances = {absence + attendance};
-
-        // when 
-        AttendanceBook attendanceBook = AttendanceBook.of(attendances);
-
-        // then 
-        assertThat(attendanceBook.getScore(0)).isEqualTo(0);
-    }
-
-    @Test
-    @DisplayName("점수가 음수가 될 경우 0점으로 처리한다.")
-    void getMinusScore() {
-        // given 
-        String[] attendances = {"P"};
-        
-        // when
-        AttendanceBook attendanceBook = AttendanceBook.of(attendances);
-        
-        // given 
-        assertThat(attendanceBook.getScore(0)).isEqualTo(0);
-    }
-
-    private String generateAttendances(String type, int count) {
-        return Stream.generate(() -> type).limit(count)
-                                    .reduce((a, b) -> a + b)
-                                    .get();
     }
 }
